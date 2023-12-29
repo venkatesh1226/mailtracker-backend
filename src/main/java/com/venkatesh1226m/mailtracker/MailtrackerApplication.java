2983975060791398087878bebaa96a0f239a8689
venkatesh1226 <venkatesh1226m@gmail.com>
2023-12-29 12:16:15 -0800

    first commit

---
+++ src/main/java/com/venkatesh1226m/mailtracker/MailtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.venkatesh1226m.mailtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venkatesh1226m/mailtracker/MailtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatesh1226m.mailtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
